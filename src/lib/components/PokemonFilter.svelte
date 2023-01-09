c871f9a0e7c79b68a560f344acfe24d5f6a02597
João Marcos <jmarcossferreira@hotmail.com>
2023-01-09 08:04:07 -0300

    Create pokemon filter

---
+++ src/lib/components/PokemonFilter.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let filter = '';
+</script>
+
+<div class="flex gap-2">
+	<input
+		class="input w-full border border-base-200"
+		placeholder="Filter pokemon..."
+		type="text"
+		name="filter"
+		id="filter"
+		bind:value={filter}
+	/>
+	<button class="btn btn-primary" type="submit" disabled={!filter.length}>Filter</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import PokemonFilter from '$lib/components/PokemonFilter.svelte';
 	import PokemonGrid from '$lib/components/PokemonGrid.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
+<PokemonFilter />
 <PokemonGrid pokemons={data.pokemons} />
